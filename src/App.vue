758d46a67cb94ccaf0cf2e5f2d832060e952bc89
neerajsharmakr047@gmail.com <neerajsharmakr047@gmail.com>
2023-09-25 19:06:19 +0530

    Create input field and add clear button and caps button

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+
+  <TestApp />
+</template>
+
+<script>
+import TestApp from "./components/TestApp.vue";
+
+export default {
+  name: "App",
+  components: { TestApp },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TestApp.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1>Test Component</h1>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+  <div id="testing">
+    <h1>testing</h1>
+
+    <h1>{{ name }}</h1>
+    <br />
+
+    <input type="text" v-model="name" /><br />
+  </div>
+  <br />
+  <div id="btn">
+    <button v-on:click="clear">clear</button>
+    <button v-on:click="add">Capitilize</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TestApp.vue",
+
+  data() {
+    return {
+      msg: "Hello Neeraj",
+      name: "",
+    };
+  },
+  methods: {
+    clear: function () {
+      this.name = "";
+    },
+    add: function () {
+      this.name = this.name.toUpperCase();
+    },
+  },
+};
+</script>
+
+<style></style>
